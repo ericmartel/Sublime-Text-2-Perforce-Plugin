3832c262c9cc236d2b419aa80df1a5d85dc0d6f2
Eric Martel <emartel@gmail.com>
2012-01-07 16:50:16 -0500

    Added check to see if file is in depot

--- Perforce.py
+++ Perforce.py
@@ -1,4 +1,7 @@
+# Written by Eric Martel (emartel@gmail.com / www.ericmartel.com)
 # Inspired by https://gist.github.com/1065808
+
+# to add a keyboard shortcut, call perforce_checkout
 
 import sublime
 import sublime_plugin
@@ -7,14 +10,41 @@ import os
 import stat
 import subprocess
 
-def Checkout(filename):
-    filestats = os.stat(filename)[0];
+warningsenabled = False;
+
+def Checkout(in_filename):
+    filestats = os.stat(in_filename)[0];
     if(filestats & stat.S_IWRITE):
         return -1, "File is already writable."
-    
-    folder_name, filename = os.path.split(filename);
-    command = 'p4 edit ' + filename
 
+    folder_name, filename = os.path.split(in_filename)
+
+    # check if the file is in the depot
+    command = 'p4 info'
+    p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=folder_name, shell=True)
+    result, err = p.communicate()
+
+    if(err):
+        return 0, err.strip()
+    
+    # locate the line containing "Client root: " and extract the following path
+    startindex = result.find("Client root: ")
+    if(startindex == -1):
+        return -1, "Unexpected output from 'p4 info'."
+
+    startindex += 13 # advance after 'Client root: '
+    endindex = result.find("\n", startindex)
+    if(endindex == -1):
+        return -1, "Unexpected output from 'p4 info'."
+
+    clientroot = result[startindex:endindex].strip()
+    clientrootindex = folder_name.lower().find(clientroot.lower());
+    
+    if(clientrootindex == -1):
+        return -1, "File is not under client's root " + clientroot
+    
+    # check out the file
+    command = 'p4 edit ' + filename
     p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, cwd=folder_name, shell=True)
     result, err = p.communicate()
 
@@ -27,13 +57,19 @@ def Checkout(filename):
 class PerforceAutoCheckout(sublime_plugin.EventListener):  
     def on_pre_save(self, view):  
         if(view.is_dirty()):
-            success, message = Checkout(view.file_name());
+            success, message = Checkout(view.file_name())
             if(success >= 0):
-                print "Perforce:", message;
+                print "Perforce:", message
+            else:
+                if(warningsenabled):
+                    print "Perforce [warning]:", message
 
 class PerforceCheckoutCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         if(self.view.file_name()):
-            success, message = Checkout(self.view.file_name());
+            success, message = Checkout(self.view.file_name())
             if(success >= 0):
-                print "Perforce:", message;
+                print "Perforce:", message
+            else:
+                if(warningsenabled):
+                    print "Perforce [warning]:", message
