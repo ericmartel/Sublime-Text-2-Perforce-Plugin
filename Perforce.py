e48f1462aed41fde6498a8e01e56d12fab5f521c
Jonathan Felchlin <jonathanf@zillow.com>
2013-11-12 18:03:57 -0800

    Sourcing p3env file set in preferences

--- Perforce.py
+++ Perforce.py
@@ -51,12 +51,17 @@ perforceplugin_dir = os.getcwdu()
 
 # Utility functions
 def ConstructCommand(in_command):
+    perforce_settings = sublime.load_settings('Perforce.sublime-settings')
+    p4Env = perforce_settings.get('perforce_p4env')
     command = ''
-    if(sublime.platform() == "osx"):
+    if(p4Env and p4Env != ''):
+        command = 'source ' + p4Env + ' && '
+    elif(sublime.platform() == "osx"):
         command = 'source ~/.bash_profile && '
     # Revert change until threading is fixed
     # command = getPerforceConfigFromPreferences(command)
-    command += in_command
+    command += in_command + ' && p4 tickets'
+    LogResults(True, command)
     return command
 
 def getPerforceConfigFromPreferences(command):
